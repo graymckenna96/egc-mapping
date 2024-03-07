9f2cb83ce2168dc5fcc3ec413af5569d471516e1
Gray McKenna <gray@restorationfund.org>
2024-03-06 16:07:12 -0800

    Map three different years of ECG data

--- scripts/egc_data_prep.R
+++ scripts/egc_data_prep.R
@@ -7,7 +7,7 @@
 #### LOAD DATA AND LIBRARIES ####
 
 library(tidyverse)
-library(sf)
+library(maps)
 
 df <- read.csv("./data/observations-408160.csv")
 
@@ -35,7 +35,7 @@ obs.year.plot + geom_bar(stat="identity", fill="#a8a32f") +
   theme_bw() +
   xlab("Year") +
   ylab("Total Observations")
-ggsave("./figures/all_observations_by_year.png")
+#ggsave("./figures/all_observations_by_year.png")
 
 # Most observations are after 2015
 
@@ -49,7 +49,7 @@ obs.grade.plot + geom_bar(stat = "identity") + xlab("Quality Grade") + ylab("N O
   scale_fill_manual(values = c("#2FA8A3", "#A32FA8","#a8a32f")) + 
   theme(legend.position = "none")
 
-ggsave("./figures/all_observations_by_quality.png")
+#ggsave("./figures/all_observations_by_quality.png")
 
 # Most are research grade so we will filter to just research grade observations
 
@@ -57,6 +57,42 @@ df <- df %>% filter(quality_grade == "research")
 
 #### MAP DATA ####
 
+# Filter out any rows with NAs for lat long
+
+df <- df %>% drop_na(latitude, longitude)
+
 # Create layers per year
 
 df_years <- split(df, df$obs_year)
+
+# Start by mapping 1 year just to create initial workflow
+#Create a base plot with gpplot2
+p <- ggplot() + coord_fixed() +
+  xlab("") + ylab("")
+
+#Add map to base plot
+base_world <- p + geom_polygon(data=world_map, aes(x=long, y=lat, group=group), 
+                                     colour="#ded6ce", fill="#fdfaf7") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+   panel.background = element_rect(fill = '#add3ed', colour = 'white'), 
+   axis.line = element_line(colour = "white"), legend.position="none",
+   axis.ticks=element_blank(), axis.text.x=element_blank(),
+   axis.text.y=element_blank())
+
+# Plot the 2015 observations
+ecg_2015 <- base_world + 
+  geom_point(data=df_years[['2015']], aes(x=longitude, y=latitude), color="#5f7141", fill="#a8a32f",
+             size=4, alpha=I(0.4)) 
+
+ecg_2015
+
+# Crabs are already being observed well outside of native range. Let's check out an earlier year.
+
+ecg_2005 <- base_world + 
+  geom_point(data=df_years[['2005']], aes(x=longitude, y=latitude), color="#5f7141", fill="#a8a32f",
+             size=4, alpha=I(0.4)) 
+
+ecg_2005
+
+# Crabs present in new england, US, but still mostly in Europe
+
