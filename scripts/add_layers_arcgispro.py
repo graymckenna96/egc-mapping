c52e7379285ad6ad96b256171b17b5c3a7deece7
graymckenna96 <gray@restorationfund.org>
2024-03-06 17:01:26 -0800

    Python chunk to bulk add csv outputs from egc_data_prep script to ArcGIS Pro project gdb. *Note file paths not relative

---
+++ scripts/add_layers_arcgispro.py
@@ -0,0 +1,18 @@
+# Bulk add layers to map in ArcGIS Pro
+
+# Note: copy and paste annual csvs output from ecg_data_prep.R script to a Pro project
+
+import arcpy
+import os
+
+arcpy.env.workspace = r"C:\Users\graym\Documents\ArcGIS\Projects\EuropeanGreenCrab"
+
+# save wd to a variable
+wd = "C:\\Users\\graym\\Documents\\ArcGIS\\Projects\\EuropeanGreenCrab\\"
+
+# List all the csv files in the workspace
+csv_list = arcpy.ListFiles("*.csv")
+
+# Loop through and add all to map 
+for i in csv_list:
+    arcpy.management.XYTableToPoint(wd + i, i, "latitude", "longitude")
